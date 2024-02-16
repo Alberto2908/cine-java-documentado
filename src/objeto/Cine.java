71edd8e4ecdb0405c2b7bc2534dfb176f0a17616
Alberto2908 <alberto.cabello95@gmail.com>
2024-02-16 10:56:20 +0100

    (feat): Añado getters y setters clase cine

--- src/objeto/Cine.java
+++ src/objeto/Cine.java
@@ -10,4 +10,23 @@ public class Cine {
         this.aforo = aforo;
         this.salas = new Sala[numSalas];
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getAforo() {
+        return aforo;
+    }
+    public void setAforo(int aforo) {
+        this.aforo = aforo;
+    }
+    public Sala[] getSalas() {
+        return salas;
+    }
+    public void setSalas(Sala[] salas) {
+        this.salas = salas;
+    }
 }
