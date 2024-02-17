47a0c4e9e6322c3a6b737387727501bb0ef8fc8c
Alberto2908 <alberto.cabello95@gmail.com>
2024-02-17 10:39:47 +0100

    (feat): Testeando el menú principal del main y sus funciones

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,17 @@
 package main;
 
+import java.util.Scanner;
+
 import objeto.Cine;
 import objeto.Pelicula;
 import objeto.Sala;
 import objeto.Butaca;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean seguir = true;
+        int opcion;
 
         Cine cine1 = new Cine("Callao", 6);
         Sala sala1 = new Sala(1, new Pelicula("Cars", 117), 12, 16);
@@ -22,6 +28,26 @@ public class Main {
         cine1.añadirSala(sala5);
         cine1.añadirSala(sala6);
 
-        System.out.println(cine1.salas[2].getPelicula().getTitulo());
+        while (seguir) {
+
+            System.out.print("¿Qué operación deseas realizar?\n" + "1 - Ver salas y datos de películas\n"
+                    + "2 - Elegir sala y butaca para reservar\n"
+                    + "3 - Ver matriz con asientos libres/ocupados de una sala\n" + "4 - Salir\n" + ">> ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    cine1.mostrarSalasYPeliculas();
+                    break;
+                case 4:
+                    System.out.println("Saliendo del programa. ¡Hasta luego!");
+                    seguir = false;
+                    scanner.close();
+                    break;
+                default:
+                    System.out.println("Opción no válida. Inténtalo de nuevo." + "\n");
+
+            }
+        }
     }
 }
